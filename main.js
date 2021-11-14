ea5f263f6ec1f88b0ccd7256f86545e15a2d27b0
AdrielDatario <79733895+AdrielDatario@users.noreply.github.com>
2021-11-13 18:41:48 -0800

    error

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 video = "";
-status = "";
+objects = [];
+flag = "";
 
 function preload(){
     video = createVideo('video.mp4')
 }
-function sutup(){
+function setup(){
     canvas = createCanvas(480, 380);
     canvas.center();
     video.hide();
@@ -17,8 +18,33 @@ function start(){
 
 function modelLoaded(){
     console.log("Model Loaded!");
-    status = true;
+    flag = true;
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResults(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+    if(flag != ""){
+        objectDetector.detect(video, gotResults);
+        for (i = 0; 1< objects.length; i++);
+        document.getElementById("status").innerHTML = "Status : Object Detected"
+        document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :"+objects.length;
+
+        fill("#FF0000")
+        percent = floor(object[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#FF0000")
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+    }
 }
